84c22eaeec25e8d0225faa77f93460be9d9c7279
Aren Marsden <aren.marsden6@gmail.com>
2024-05-24 23:49:47 +0100

    Yeet

--- smple-plugin/src/main/java/org/ammonium/smple/SmplePlugin.java
+++ smple-plugin/src/main/java/org/ammonium/smple/SmplePlugin.java
@@ -1,10 +1,18 @@
 package org.ammonium.smple;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.ammonium.smple.command.ExampleCommand;
 import org.ammonium.smple.sdk.command.CommandManager;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SmplePlugin extends JavaPlugin {
+
+    private final MiniMessage miniMessage = MiniMessage.miniMessage();
 
     @Override
     public void onLoad() {
@@ -17,6 +25,17 @@ public final class SmplePlugin extends JavaPlugin {
             .withCommands(
                 new ExampleCommand()
             );
+
+        Bukkit.getScheduler().runTaskTimer(this, () -> {
+            Bukkit.broadcast(
+                Component.text("Hello!")
+                    .decorate(TextDecoration.BOLD, TextDecoration.ITALIC)
+                    .color(NamedTextColor.AQUA)
+                    .clickEvent(ClickEvent.clickEvent(
+                        ClickEvent.Action.RUN_COMMAND, "/example")),
+                "smple.example");
+
+        }, 20, 20);
     }
 
     @Override
